5ec047dbc8cb458672ac2dc69c9b42ae348361ca
juliapalm <32334112+juliapalm@users.noreply.github.com>
2017-11-24 15:02:31 +0100

    Head and feet

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer>
+  <p>©The golden trio <?php echo date(Y); ?></p>
+</footer>
+
+<?php wp_footer() ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+
+<head>
+		<title>Herrtorps Qvarn</title>
+		<meta charset="utf-8"/>
+		<meta name="viewport" content="initial-scale=1, width=device-width"/>
+		<?php wp_head(); ?>
+    <link rel="stylesheet" href="<?php echo bloginfo('stylesheet_url') ?>">
+</head>
+
+<?php // require('partials/sitehead.php');?>
+
+<body>
