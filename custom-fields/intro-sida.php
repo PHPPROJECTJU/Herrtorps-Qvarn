8e0cf48efd0c85593f74d528a94b4438d8dcc8d6
juliapalm <32334112+juliapalm@users.noreply.github.com>
2017-11-28 15:43:12 +0100

    Update

--- custom-fields/intro-sida.php
+++ custom-fields/intro-sida.php
@@ -4,7 +4,7 @@
 
         <div class="top-img"><?php the_post_thumbnail('top_img');?></div>
         <h2><?php the_field('text'); ?></h2>
-        <p><?php the_field('textarea'); ?></p>
+        <p><?php the_field('text_field'); ?></p>
 
         <?php
   endif;
