00d1f16abab89300a6ac0751a915fdbd305999da
elbrag <ellenbrage@outlook.com>
2017-11-30 19:27:04 +0100

    m

--- omgivning.php
+++ omgivning.php
@@ -14,7 +14,8 @@ if( have_posts() ) {
     while ( have_posts() ) {
       the_post();?>
 
-      <div class="omgivning">
+      <section class="omgivning">
+        <div class="contentwidth">
           <h2><?php the_field('skararubrik'); ?></h2>
 
             <?php
@@ -23,34 +24,48 @@ if( have_posts() ) {
             $mlbild = $skarabild['sizes'][ $mediumlarge ];
             $width = $skarabild['sizes'][ $mediumlarge . '-width' ];
             $height = $skarabild['sizes'][ $mediumlarge . '-height' ];
-            //
+
              if ($skarabild) { ?>
-            <div class='omgivning_bild' style='background-image: url("<?php echo $mlbild;?>");'> 
+            <div class='omgivning_bild' style='background-image: url("<?php echo $mlbild;?>");'></div>
             <?php } ?>
 
+          <div class="omgivning_text">
+              <p><?php the_field('skaratext'); ?></p>
 
-          <p><?php the_field('skaratext'); ?></p>
-          <p><?php the_field('skaralank'); ?></p>
-      </div>
+              <div class='btncontainer'>
+                <a target="_blank" href=<?php the_field('skaralank');?>><button class='outlinebtn_beige'>Läs mer</button></a>
+              </div>
+          </div>
 
-      <div class="omgivning">
+
+        </div>
+      </section>
+
+      <section class="omgivning">
+        <div class="contentwidth">
           <h2><?php the_field('hornborgarubrik'); ?></h2>
 
             <?php
-            // $mediumlarge = 'medium_large';
-            // $hornbild = get_field('hornborgabild');
-            // $mlbild = $hornbild['sizes'][ $mediumlarge ];
-            // $width = $hornbild['sizes'][ $mediumlarge . '-width' ];
-            // $height = $hornbild['sizes'][ $mediumlarge . '-height' ];
-            //
-            // if ($hornbild) { ?>
-            <!--<div class="hornbild" style='background-image: url("<?php //echo $hornbild;?>");'></div>-->
-            <?php //} ?>
+            $mediumlarge = 'medium_large';
+            $hornbild = get_field('hornborgabild');
+            $mlbild = $hornbild['sizes'][ $mediumlarge ];
+            $width = $hornbild['sizes'][ $mediumlarge . '-width' ];
+            $height = $hornbild['sizes'][ $mediumlarge . '-height' ];
 
-        <?php the_field('hornborgabild') ?>
-        <p><?php the_field('hornborgatext'); ?></p>
-        <p><?php the_field('hornborgalank'); ?></p>
-      </div>
+            if ($hornbild) { ?>
+            <div class='omgivning_bild' style='background-image: url("<?php echo $mlbild;?>");'></div>
+            <?php } ?>
+
+          <div class="omgivning_text">
+              <p><?php the_field('hornborgatext'); ?></p>
+
+              <div class='btncontainer'>
+                <a target="_blank" href=<?php the_field('hornborgalank');?>><button class='outlinebtn_beige'>Läs mer</button></a>
+              </div>
+          </div>
+
+        </div>
+      </section>
 
     <?php }
 }
