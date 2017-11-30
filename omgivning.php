4777c73da26ef6b2fffd56ab96653ab885123fb7
juliapalm <32334112+juliapalm@users.noreply.github.com>
2017-11-30 11:36:34 +0100

    ...

--- omgivning.php
+++ omgivning.php
@@ -15,17 +15,41 @@ if( have_posts() ) {
       the_post();?>
 
       <div class="omgivning">
-        <h2><?php the_field('Skararubrik'); ?></h2>
-        <p><?php the_field('Skaratext'); ?></p>
-        <?php the_field('Skarabid'); ?>
-        <p><?php the_field('Skaralänk'); ?></p>
+          <h2><?php the_field('skararubrik'); ?></h2>
+
+            <?php
+            // $mediumlarge = 'medium_large';
+            // $skarabild = get_field('skarabild');
+            // $mlbild = $skarabild['sizes'][ $mediumlarge ];
+            // $width = $skarabild['sizes'][ $mediumlarge . '-width' ];
+            // $height = $skarabild['sizes'][ $mediumlarge . '-height' ];
+            //
+            // if ($skarabild) { ?>
+            <!--<div class="skarabild" style='background-image: url("<?php //echo $skarabild;?>");'></div>-->
+            <?php //} ?>
+
+          <?php the_field('skarabild'); ?>
+          <p><?php the_field('skaratext'); ?></p>
+          <p><?php the_field('skaralank'); ?></p>
       </div>
 
       <div class="omgivning">
-        <h2><?php the_field('Hornborgarubrik'); ?></h2>
-        <p><?php the_field('Hornborgatext'); ?></p>
-        <?php the_field('Hornborgabild'); ?>
-        <p><?php the_field('Hornborgalänk'); ?></p>
+          <h2><?php the_field('hornborgarubrik'); ?></h2>
+
+            <?php
+            // $mediumlarge = 'medium_large';
+            // $hornbild = get_field('hornborgabild');
+            // $mlbild = $hornbild['sizes'][ $mediumlarge ];
+            // $width = $hornbild['sizes'][ $mediumlarge . '-width' ];
+            // $height = $hornbild['sizes'][ $mediumlarge . '-height' ];
+            //
+            // if ($hornbild) { ?>
+            <!--<div class="hornbild" style='background-image: url("<?php //echo $hornbild;?>");'></div>-->
+            <?php //} ?>
+
+        <?php the_field('hornborgabild') ?>
+        <p><?php the_field('hornborgatext'); ?></p>
+        <p><?php the_field('hornborgalank'); ?></p>
       </div>
 
     <?php }
