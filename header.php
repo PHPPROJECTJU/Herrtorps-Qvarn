c5a51920a000600b5b7dd6a92fc51f96470df0d6
Albinjoe <albinjoh1@gmail.com>
2017-12-01 14:33:05 +0100

    meta tags keywords.

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="KEYWORDS" content="Upplevelser, upplevelsepaket, fiske, fiskepaket, naturupplevelse, safari, paddling, kanot, lägenheter, hotell, vandrarhem, konferens, kvarn, qvarn, stuga, cafe, café, restaurang, festarrangör, boule, femkamp, Västergötland, Skara">
     <!--  No title tag at all -->
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>">
     <link href="https://fonts.googleapis.com/css?family=Nunito:300,300i,400,400i,600,600i,800,800i" rel="stylesheet">
