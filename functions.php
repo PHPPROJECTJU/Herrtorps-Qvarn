ce0587054a0d18ebd522721130f61750912de853
Albinjoe <albinjoh1@gmail.com>
2017-12-01 12:04:53 +0100

    css menu fortsättning

--- functions.php
+++ functions.php
@@ -30,4 +30,14 @@ function mytheme_enqueue_style() {
 }
 add_action( 'wp_enqueue_scripts', 'mytheme_enqueue_style' );
 
+/*-----To make menu object active https://stackoverflow.com/questions/26789438/how-to-add-active-class-to-wp-nav-menu-current-menu-item-simple-way----*/
+add_filter('nav_menu_css_class' , 'special_nav_class' , 10 , 2);
+
+function special_nav_class ($classes, $item) {
+    if (in_array('current-menu-item', $classes) ){
+        $classes[] = 'active ';
+    }
+    return $classes;
+}
+
 ?>
