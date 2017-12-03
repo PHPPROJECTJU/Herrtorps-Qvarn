b0aa506d63fcc98299ec15af749060ba55598a17
Albinjoe <albinjoh1@gmail.com>
2017-12-03 19:33:17 +0100

    lite menyfix

--- partials/sitehead.php
+++ partials/sitehead.php
@@ -2,19 +2,14 @@
     <!-- 27 nov 2017 https://css-tricks.com/forums/topic/menu-split-by-logo/ -->
     <nav>
         <div class="menu">
-            <?php wp_nav_menu( array('menu' => 'Menu left', 'container' => '', 'items_wrap' => '<ul class="menuleft">%3$s</ul>' )); ?>
+            <?php wp_nav_menu( array('menu' => 'Menu', 'container' => '', 'items_wrap' => '<ul class="menuleft">%3$s</ul>' )); ?>
 
                 <a href="<?php echo home_url();?>"><div id='logo'></div></a>
-
-            <?php wp_nav_menu( array('menu' => 'Menu right', 'container' => '', 'items_wrap' => '<ul class="menuright">%3$s</ul>' )); ?>
             <div class="extras">
                 <a href="/opening-hours">
                     <div class="opening-icon">
                     </div>
                 </a>
-                <div class="language-icon">
-                    <a href="language-hours"></a>
-                </div>
             </div>
             <div id="hamburger" onclick="openNav(this)" title="Menu">&#9776;</div>
         </div>
