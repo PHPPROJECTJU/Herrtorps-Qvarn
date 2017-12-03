1f5ad24400348a9aeecb345984e54f9da4239f3b
elbrag <32334193+elbrag@users.noreply.github.com>
2017-12-03 15:08:17 +0100

    ö

--- includes/reg_media.php
+++ includes/reg_media.php
@@ -5,6 +5,7 @@
   add_image_size( 'grid_thumbnail', 200, 200, true );
   add_image_size( 'single_large', 660, 400, false );
   add_image_size( 'top_img', 2000, 800, false );
+  add_image_size( 'top_img_full', 2000, 1300, false );
 
   //Activate Featured Image
   add_theme_support('post-thumbnails');
