d0956621f30c5f76e223ea9441ca2df9b644ac00
elbrag <32334193+elbrag@users.noreply.github.com>
2017-12-05 11:55:46 +0100

    k

--- 404.php
+++ 404.php
@@ -1,1 +1,15 @@
-This page is not available. Please go back to previous page.
+<?php get_header();?>
+
+<main>
+
+<section class='lightsection' style='padding: 40vh 0'>
+  <div class='contentwidth' style='text-align: center;'>
+    <h2>Sidan du söker verkar inte finnas.</h2>
+    <a href="/hem"><h3>Gå till Hem</h3></a>
+  </div>
+</section>
+
+
+</main>
+
+<?php get_footer(); ?>
