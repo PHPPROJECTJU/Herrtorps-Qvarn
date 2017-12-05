8dd8dc752cd0c2793e07eced150c42b4c5751c6a
Albinjoe <albinjoh1@gmail.com>
2017-12-05 14:47:14 +0100

    menu fix igen

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,21 +1,7 @@
 var menuleft = document.querySelector(".menuleft");
-var menuright = document.querySelector(".menuright");
-var hamburger = document.getElementById("hamburger");
-// function openNav() {
-// 		menuleft.style.display = "block";
-//     menuright.style.display = "block";
-// }
 
-$(window).resize(function() {
     var subitem1 = document.querySelector(".sub-menu");
     var subitem2 = document.querySelectorAll(".sub-menu")[1];
-
-        if ($(this).width() >= 960) {
-
-          subitem1.style.display == "none";
-          subitem2.style.display == "none";
-
-        } else if($(this).width() < 960){
 
             document.querySelector(".sub-menu-1").addEventListener("click", function(){
                 if(subitem1.style.display == "block"){
@@ -31,19 +17,15 @@ $(window).resize(function() {
                 } else {
                     subitem2.style.display = "block";
                 }
-            });
-        }
-});
+            });    
 
 function openNav(x) {
     x.classList.toggle("change");
 
     if(menuleft.style.display == "block"){
       menuleft.style.display = "none";
-      menuright.style.display = "none";
     } else {
       menuleft.style.display = "block";
-      menuright.style.display = "block";
     }
 
 }
