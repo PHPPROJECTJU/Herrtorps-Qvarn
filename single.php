5c03ef1b0bd21ab37238e9c448f74301a7e41b5a
Albinjoe <albinjoh1@gmail.com>
2017-12-10 18:05:07 +0100

    single aktiviteter

--- single.php
+++ single.php
@@ -5,27 +5,32 @@
 
 if( have_posts() ) {
 
-  echo "<div class='headerimage'>";
-      the_post_thumbnail( 'top_img' );
-  echo "</div>";
+
 
     while ( have_posts() ) {
 
       the_post();
+
+      echo "<div class='headerimage'>";
+      echo get_the_post_thumbnail($post_id,'top_img');
+      echo "</div>";
           ?>
 
           <div class="aktivitet">
               <div class="contentwidth">
+                  <div class="aktivitet-box">
+                      <h2 class='page_rubrik'><?php the_field('aktivitet'); ?></h2>
+                      <p class="single-paragraf"><?php the_field('beskrivning'); ?></p>
+                  </div>
+                  <div class="price-box">
+                    <h3><?php echo get_field('pris-rubrik');?></h3>
+                    <?php
+                        echo get_field('pris');
+                    ?>
+                  </div>
 
-                  <h2 class='page_rubrik'><?php the_field('aktivitet'); ?></h2>
-                  <p><?php the_field('beskrivning'); ?></p>
-                  <?php
-                  $pris = get_field('pris');
 
-                  if ($pris) {?>
-                    <p>Pris: <?php echo $pris ?>kr/person</p>
-                  <?php }
-                  ?>
+
 
 
               </div>
