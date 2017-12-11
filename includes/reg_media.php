15b5661f3036364c54db4708bd72bcf5cc240f7f
Albinjoe <albinjoh1@gmail.com>
2017-12-11 17:31:24 +0100

    grid @media

--- includes/reg_media.php
+++ includes/reg_media.php
@@ -2,7 +2,7 @@
   //Defining image sizes to save on server
 
   //Width, height, crop
-  add_image_size( 'grid_thumbnail', 200, 200, true );
+  add_image_size( 'grid_thumbnail', 300, 300, true );
   add_image_size( 'single_large', 660, 400, false );
   add_image_size( 'top_img', 2000, 800, false );
   add_image_size( 'top_img_full', 2000, 1300, false );
